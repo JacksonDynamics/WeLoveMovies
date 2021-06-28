8dc87cbe136e85acf212f9087e9c3d13d5e4d4d1
Brendon Jackson <bjackso3@hotmail.com>
2021-06-28 15:30:54 -0600

    Added Error files

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 if (process.env.USER) require("dotenv").config();
+
 const express = require("express");
 const app = express();
-
 const cors =require("cors")
+
+const errorHandler = require("./errors/errorHandler")
+const notFound = require("./errors/notFound")
 
 app.use(express.json)
 app.use(cors())
 
+
+// Not found Handler
+app.use(notFound)
+
+// Error handler
+app.use(errorHandler)
 
 module.exports = app;
